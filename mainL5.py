428f5694f9b4dfb15a711eb9eea665a3ef3861e4
Dante Espinoza <dante.espinoza@gmail.com>
2022-10-06 22:07:11 +0200

    creción de la estructura principal del proyecto

---
+++ mainL5.py
@@ -0,0 +1,13 @@
+""" Actividad relacionada con la lección 5:
+=========================================================================
+
+Si no tiene cuenta en GitHub, cree una. A continuación debe crear un nuevo
+repositorio con el nombre de la asignatura BPP.
+Integre GitHub con Visual Studio Code tal y como hemos visto en clase. A
+continuación, clone el repositorio que acaba de crear de la asignatura BPP.
+En dicho repositorio debe subir todos los códigos que haya ido realizando en
+las lecciones anteriores. Trate de subir cada uno de los documentos de forma
+escalonada, para que pueda interactuar con diferentes commits y visualizar
+el histórico de sus ficheros.
+Debe entregar un documento en el que se muestre su usuario en GitHub
+para poder comprobar el contenido que ha subido. """
